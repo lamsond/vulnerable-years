9152498054fbaacfebd3780572c4b2cd4a77a2b3
conrad <lamsond@wctech.org>
2019-05-25 09:36:37 -0400

    set up sample pages

---
+++ gatsby_crash_course/src/pages/services.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ServicesPage = () => {
+	return (
+			<div>
+				<h1>Services</h1>
+				<p>i woke up in the morning and i got myself a beer</p>
+			</div>
+			);
+}
+
+export default ServicesPage;
+
+
