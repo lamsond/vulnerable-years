e6777f3f3debdae4105a60479302c8b9515b5c8e
conrad <lamsond@wctech.org>
2019-05-27 11:57:24 -0400

    get blog posts working

--- gatsby_crash_course/src/templates/blog-post.js
+++ gatsby_crash_course/src/templates/blog-post.js
@@ -1,31 +1,33 @@
-import React from 'react';
+import React from 'react'
+import Link from 'gatsby-link'
+import Layout from '../components/layout.js';
 
-export default function Template({data}){
-	const post = data.markdownRemark;
+export default function Template({ data }) {
+	  const post = data.markdownRemark
 
-	return(
-			<div>
-				<Link to='/blog'>Go Back</Link>
-				<hr />
-				<h1>{post.frontmatter.title}</h1>
-				<h4>Posted by {post.frontmatter.author} on {post.frontmatter.date}</h4>
-				<div dangerouslySetInnerHTML={{__html: post.html}} />
-			</div>
-		)
+			  return (
+						    <Layout>
+								      <Link to="/blog">Go Back</Link>
+											      <hr />
+														      <h1>{post.frontmatter.title}</h1>
+																	      <h4>
+																				        Posted by {post.frontmatter.author} on {post.frontmatter.date}
+																								      </h4>
+																											      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+																														    </Layout>
+																																  )
 }
 
 export const postQuery = graphql`
-	query BlogPostByPath($path: String!){
-		markdownRemark(frontmatter: {path: { eq: $path } }){
-			html
-			frontmatter{
-				path
-				title
-				author
-				date
-			}
-		}
-	}
-
+  query BlogPostByPath($path: String!) {
+		    markdownRemark(frontmatter: { path: { eq: $path } }) {
+					      html
+									      frontmatter {
+													        path
+																		        title
+																						        author
+																										        date
+																														      }
+								    }
+				  }
 `
-
